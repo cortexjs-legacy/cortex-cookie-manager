ce8d7e6be1a86449c116f4b30f8966a7422995f7
kaelzhang <i@kael.me>
2014-05-06 15:52:09 +0800

    will encode cookie value

--- cortex.js
+++ cortex.js
@@ -1,65 +1,65 @@
-
 // console.log(document.cookie, chrome);
 
 var REGEX_MATCH_NEURON = /[^;]\s*neuron\=[^;]+/;
 
-function dispose_cookie (key) {
-    var date = new Date();
-    date.setTime(date.getTime() + -1 * 24 * 60 * 60 * 1000);
+function dispose_cookie(key) {
+  var date = new Date();
+  date.setTime(date.getTime() + -1 * 24 * 60 * 60 * 1000);
 
-    document.cookie = key + '=; path=/; expires=' + date.toGMTString();
+  document.cookie = key + '=; path=/; expires=' + date.toGMTString();
 }
 
 
-function set_cookie (key, value) {
-    document.cookie = key + '=' + value + '; path=/';
+function set_cookie(key, value) {
+  if (typeof value === 'string') {
+    value = encodeURIComponent(value);
+  }
+  document.cookie = key + '=' + value + '; path=/';
 }
 
 
 var handlers = {
-    'mode-change': function (message) {
-        var cookie = document.cookie;
+  'mode-change': function(message) {
+    var cookie = document.cookie;
 
-        if ( REGEX_MATCH_NEURON.test(cookie) ) {
-            dispose_cookie('cortex_compress');
-            dispose_cookie('cortex_combo');
-            dispose_cookie('cortex_path');
-            dispose_cookie('neuron');
+    if (REGEX_MATCH_NEURON.test(cookie)) {
+      dispose_cookie('cortex_compress');
+      dispose_cookie('cortex_combo');
+      dispose_cookie('cortex_path');
+      dispose_cookie('neuron');
 
-            send_icon_message(false);
+      send_icon_message(false);
 
-        } else {
-            set_cookie('cortex_compress', false);
-            set_cookie('cortex_combo', false);
-            set_cookie('cortex_path', 'http://localhost:9074');
-            set_cookie('neuron', 'path=http://localhost:9074/mod,ext=.js');
+    } else {
+      set_cookie('cortex_compress', false);
+      set_cookie('cortex_combo', false);
+      set_cookie('cortex_path', 'http://localhost:9074');
+      set_cookie('neuron', 'path=http://localhost:9074/mod,ext=.js');
 
-            send_icon_message(true);
-        }
-
-        location.reload();
-    },
-
-    'test-activate': function (message) {
-        send_icon_message( REGEX_MATCH_NEURON.test(document.cookie) );
+      send_icon_message(true);
     }
+
+    location.reload();
+  },
+
+  'test-activate': function(message) {
+    send_icon_message(REGEX_MATCH_NEURON.test(document.cookie));
+  }
 };
 
 
-function send_icon_message (active) {
-    chrome.extension.sendMessage({
-        event: 'set-icon',
-        active: active
-    });
+function send_icon_message(active) {
+  chrome.extension.sendMessage({
+    event: 'set-icon',
+    active: active
+  });
 }
 
 
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse){
-    var event = message.event;
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+  var event = message.event;
 
-    var handler = handlers[event];
+  var handler = handlers[event];
 
-    handler && handler(message);
+  handler && handler(message);
 });
-
-
